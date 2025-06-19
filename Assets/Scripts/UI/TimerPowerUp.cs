69ac4b8eefafd736a7451fca1f8518e8e475009f
Mica Caputo <micaelacaputo98@gmail.com>
2025-06-19 18:38:07 -0300

    Agrego powerups en UI

---
+++ Assets/Scripts/UI/TimerPowerUp.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+namespace UI
+{
+    public class TimerPowerUp : MonoBehaviour
+    {
+        [Header("Duración Power-Up")]
+        [SerializeField] private float cooldownTime;
+
+        [Header("UI")]
+        [SerializeField] private Image fill;
+
+        private float timeRemaining;
+        private bool isRunning;
+
+        void Awake()
+        {
+            gameObject.SetActive(false);
+        }
+
+        void Update()
+        {
+            if (!isRunning) return;
+            
+            timeRemaining -= Time.deltaTime;
+            fill.fillAmount = timeRemaining / cooldownTime;
+
+            if (timeRemaining <= 0f)
+            {
+                DeactivatePowerUp();
+            }
+        }
+
+        public void ActivatePowerUp()
+        {
+            timeRemaining = cooldownTime;
+            fill.fillAmount = 1f;
+            isRunning = true;
+            gameObject.SetActive(true);
+        }
+
+        private void DeactivatePowerUp()
+        {
+            isRunning = false;
+            gameObject.SetActive(false);
+        }
+    }
+}
